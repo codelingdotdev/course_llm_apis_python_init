132b8ae5d20e19ad92fb2053944e913a3dea0b81
bdelate <brendon.delate@gmail.com>
2025-12-18 10:25:37 +0200

    update starter code

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ def initialize_client(groq_api_key: str) -> Groq:
 
 def main():
     """Main function to run the chat completion."""
-    client = initialize_client(groq_api_key=os.environ.get("GROQ_API_KEY", ""))
+    client = initialize_client(groq_api_key=os.environ.get("API_KEY", ""))
 
     chat_completion = client.chat.completions.create(
         messages=[
@@ -18,7 +18,7 @@ def main():
                 "content": "Explain the importance of fast language models",
             }
         ],
-        model=os.environ.get("GROQ_ORACLE_MODEL", ""),
+        model=os.environ.get("ORACLE_MODEL", ""),
     )
 
     print(chat_completion.choices[0].message.content)
